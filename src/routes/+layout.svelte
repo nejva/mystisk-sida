20c1f347e1bdc10a3ba3ef16b896c86f372a1170
Nova Bettinger <novbet@edu.umea.se>
2026-04-27 15:07:48 +0200

    message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 		<AppBar.Lead>
 		</AppBar.Lead>
 		<AppBar.Headline class="flex justify-center">
-			<h1 class="h1"><a href={resolve("/devblog")}> No dev blog ???</a></h1>
+			<h1 class="h1"><a href={resolve("/devblog")}> dev blog till ett spel utan namn </a></h1>
 		</AppBar.Headline>
 		<AppBar.Trail class="justify-end">
 			<a href={resolve("/")} class="btn-icon hover:preset-tonal"><CircleUserIcon class="size-6" /></a>
